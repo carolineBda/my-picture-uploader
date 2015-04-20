9c39d7e006c0f2b0288c4630b84c2ccab216c983
carolineBda <caroline.bourdeu@gmail.com>
2015-04-20 11:36:42 +0100

    Add sever side check fr geo IP

--- server/api/user.js
+++ server/api/user.js
@@ -1,7 +1,19 @@
 'use strict';
 
+var request = require('request');
+var GEOIP_URL = 'https://freegeoip.net/json/';
+
 function isBodyValid(body) {
-  return (!body.firstName || !body.lastName || !body.email);
+  return (body.firstName && body.lastName && body.email);
+}
+
+function getUserIp(headerString) {
+  return headerString ? headerString.split(',')[0] : '';
+}
+
+function isFromUK(body) {
+  var geoInfo = JSON.parse(body);
+  return geoInfo && geoInfo.country_code === 'GB';
 }
 
 module.exports.create = function (req, res) {
@@ -9,5 +21,14 @@ module.exports.create = function (req, res) {
   if (!isBodyValid(body)) {
     return res.send(400);
   }
-  res.send(200);
+
+  var userIP = getUserIp(req.headers['x-forwarded-for']);
+
+  request(GEOIP_URL + userIP, function (err, httpRes, body) {
+
+    if (!isFromUK(body)) {
+      return res.send(401, {message: 'User country not allowed'});
+    }
+    return res.send(200);
+  });
 };
--- server/api/user.spec.js
+++ server/api/user.spec.js
@@ -1,15 +1,27 @@
 'use strict';
 
-var should = require('should');
 var app = require('../app');
 var request = require('supertest');
 var unroll = require('unroll');
+var nock = require('nock');
+nock.enableNetConnect('127.0.0.1');
 
-describe('POST /api/things', function() {
+describe('POST /api/user', function() {
+  var BRITISH_IP = '195.47.223.87';
 
-  it('should respond OK when all parameters sent', function(done) {
+  beforeEach(function() {
+    nock('https://freegeoip.net')
+      .get('/json/' + BRITISH_IP)
+      .reply(200, {
+        ip: BRITISH_IP,
+        country_code: 'GB'
+      });
+  });
+
+  it('should respond OK when all parameters sent and IP from GB', function(done) {
     request(app)
       .post('/api/user')
+      .set('X-Forwarded-For', BRITISH_IP)
       .send({firstName:'Jenny', lastName: 'Johns', email: 'bla'})
       .expect(200)
       .end(done);
@@ -27,4 +39,21 @@ describe('POST /api/things', function() {
     [{firstName:'Jenny', email: 'bla'}],
     [{lastName:'Jenny', email: 'bla'}]
   ]);
+
+  it('should respond Unauthorised when freegeoip respond not in UK', function(done) {
+    var FRENCH_IP = '92.222.46.207';
+    nock('https://freegeoip.net')
+      .get('/json/' + FRENCH_IP)
+      .reply(200, {
+        ip: FRENCH_IP,
+        country_code: 'FR',
+        country_name: 'France'
+      });
+
+    request(app)
+      .post('/api/user')
+      .set('X-Forwarded-For', FRENCH_IP)
+      .send({firstName:'Jenny', lastName: 'Johns', email: 'bla'})
+      .expect(401, {message: 'User country not allowed'}, done);
+  });
 });
